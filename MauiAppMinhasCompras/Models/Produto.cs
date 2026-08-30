a4cf46d0bfebcc9c88b8ebb3e1c7ed95d7c33365
kaique nunes <knunes070@gmail.com>
2026-08-30 20:07:33 -0300

    Validação da inserção de produto

--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -4,9 +4,22 @@ namespace MauiAppMinhasCompras.Models
 {
     public class Produto
     {
+        string _descricao; 
+
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public string Descricao { get; set; }
+        public string Descricao {
+            get => _descricao; 
+            set
+            {
+                if (value == null)
+                {
+                    throw new Exception("Por favor, preencha a descrição"); // Impedir que adicionem algo em branco para a lista
+                }
+
+                _descricao = value;
+            }
+        }
         public double Quantidade { get; set; }
         public double Preco { get; set; }
 
